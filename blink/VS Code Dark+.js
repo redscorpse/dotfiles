3e83bd89f8ebea5138a8a140ffc7fda26154c397
Reds Corpse <redscorpse.arf@gmail.com>
2023-02-04 01:55:59 +0100

    Update files

--- blink/VS Code Dark+.js
+++ blink/VS Code Dark+.js
@@ -1,4 +1,4 @@
 t.prefs_.set('color-palette-overrides',["#66787a", "#e9643b", "#38e9a8", "#e5b684", "#44aae5", "#f16d99", "#00d8ea", "#bcdee1", "#598489", "#f93f08", "#00ff8f", "#f68e23", "#009cff", "#ff438e", "#00ffff", "#d0fcff"]);
 t.prefs_.set('foreground-color', "#cccccc");
 t.prefs_.set('background-color', "#1d1d1d");
-t.prefs_.set('cursor-color', "#ffffff");
+t.prefs_.set('cursor-color', "#4B5632");
